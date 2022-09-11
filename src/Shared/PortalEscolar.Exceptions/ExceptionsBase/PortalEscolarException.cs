89fec5b6e6d374984f322edd290940b6af4a0eda
JacksonSM <jacksons.m2018@gmail.com>
2022-09-11 09:17:27 -0300

    Criado as exceptions customizada.

---
+++ src/Shared/PortalEscolar.Exceptions/ExceptionsBase/PortalEscolarException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace PortalEscolar.Exceptions.ExceptionsBase;
+
+[Serializable]
+public class PortalEscolarException : SystemException
+{
+    public PortalEscolarException(string mensagem) : base(mensagem)
+    {
+    }
+
+    protected PortalEscolarException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
