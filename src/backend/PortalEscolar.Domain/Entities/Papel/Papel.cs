606b652dc223b7f49bbbc86493c2d351dee09344
JacksonSM <jacksons.m2018@gmail.com>
2022-09-16 11:00:53 -0300

    Criando classes de papeis no domain.

---
+++ src/backend/PortalEscolar.Domain/Entities/Papel/Papel.cs
@@ -0,0 +1,12 @@
+﻿namespace PortalEscolar.Domain.Entities.Papel;
+public class Papel : EntityBase
+{
+    public string Nome { get; set; }
+    public string NomeNormalizado { get; set; }
+    public Papel() { }
+    public Papel(string nome)
+    {
+        Nome = nome;
+        NomeNormalizado = nome.ToUpper();
+    }
+}
---
+++ src/backend/PortalEscolar.Domain/Entities/Papel/PapelUsuario.cs
@@ -0,0 +1,7 @@
+﻿namespace PortalEscolar.Domain.Entities.Papel;
+public class PapelUsuario 
+{
+    public string EmailUsuario { get; set; }
+    public long PapelId { get; set; }
+    public Papel Papel { get; set; }
+}
