15537795ec4dcd6b470b1596e372da4b99e0e9f6
JacksonSM <jacksons.m2018@gmail.com>
2022-09-18 20:05:41 -0300

    Alterado mapeamento do campo data nascimento de todas as entidades.

--- src/backend/PortalEscolar.Application/Services/Mapping/AutoMapperConfig.cs
+++ src/backend/PortalEscolar.Application/Services/Mapping/AutoMapperConfig.cs
@@ -11,13 +11,13 @@ public class AutoMapperConfig : Profile
     public void RequestForEntity()
     {
         CreateMap<Communication.Request.RequestRegistrarDiretorJson,Domain.Entities.Diretoria.Diretor>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => DateTime.ParseExact(origem.DataNascimento, "dd/MM/yyyy", CultureInfo.InvariantCulture)));
+            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
 
         CreateMap<Communication.Request.RequestRegistrarProfessoraJson, Domain.Entities.SalaAula.Professora>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => DateTime.ParseExact(origem.DataNascimento, "dd/MM/yyyy", CultureInfo.InvariantCulture)));
+            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
 
         CreateMap<Communication.Request.RequestRegistrarAlunoJson, Domain.Entities.SalaAula.Aluno>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => DateTime.ParseExact(origem.DataNascimento, "dd/MM/yyyy", CultureInfo.InvariantCulture)));
-        
+            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
+
     }
 }
