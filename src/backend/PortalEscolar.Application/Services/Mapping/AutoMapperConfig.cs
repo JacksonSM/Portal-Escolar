1971e981a96fa6a4e329e0faff8d7116a2b66504
JacksonSM <jacksons.m2018@gmail.com>
2022-09-18 21:20:08 -0300

    retirando mapeamento forçado da data nascimento no automapper.

--- src/backend/PortalEscolar.Application/Services/Mapping/AutoMapperConfig.cs
+++ src/backend/PortalEscolar.Application/Services/Mapping/AutoMapperConfig.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using System.Globalization;
 
 namespace PortalEscolar.Application.Services.Mapping;
 public class AutoMapperConfig : Profile
@@ -10,14 +9,14 @@ public class AutoMapperConfig : Profile
     }
     public void RequestForEntity()
     {
-        CreateMap<Communication.Request.RequestRegistrarDiretorJson,Domain.Entities.Diretoria.Diretor>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
+        CreateMap<Communication.Request.RequestRegistrarDiretorJson, Domain.Entities.Diretoria.Diretor>();
 
-        CreateMap<Communication.Request.RequestRegistrarProfessoraJson, Domain.Entities.SalaAula.Professora>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
 
-        CreateMap<Communication.Request.RequestRegistrarAlunoJson, Domain.Entities.SalaAula.Aluno>()
-            .ForMember(c => c.DataNascimento, opt => opt.MapFrom(origem => origem.DataNascimento));
+        CreateMap<Communication.Request.RequestRegistrarProfessoraJson, Domain.Entities.SalaAula.Professora>();
+
+
+        CreateMap<Communication.Request.RequestRegistrarAlunoJson, Domain.Entities.SalaAula.Aluno>();
+
 
     }
 }
