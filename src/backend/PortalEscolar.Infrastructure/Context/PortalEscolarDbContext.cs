cfa3f6cc0ca183a0903da65e2d1a784752893be5
JacksonSM <jacksons.m2018@gmail.com>
2022-09-20 17:27:13 -0300

    Aplicado novas tabelas turma e matricula no banco de dados.

--- src/backend/PortalEscolar.Infrastructure/Context/PortalEscolarDbContext.cs
+++ src/backend/PortalEscolar.Infrastructure/Context/PortalEscolarDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PortalEscolar.Domain.Entities.Diretoria;
-using PortalEscolar.Domain.Entities.Diretoria.Matricula;
 using PortalEscolar.Domain.Entities.SalaAula;
 
 namespace PortalEscolar.Infrastructure.Context;
@@ -11,8 +10,6 @@ public class PortalEscolarDbContext : DbContext
     public DbSet<Diretor> Diretor { get; set; }
     public DbSet<Professora> Professora { get; set; }
     public DbSet<Aluno> Aluno { get; set; }
-    public DbSet<Turma> Turma { get; set; }
-    public DbSet<Matricula> Matricula { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
--- src/backend/PortalEscolar.Infrastructure/Migrations/PortalEscolarDbContextModelSnapshot.cs
+++ src/backend/PortalEscolar.Infrastructure/Migrations/PortalEscolarDbContextModelSnapshot.cs
@@ -55,14 +55,14 @@ namespace PortalEscolar.Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Diretor", (string)null);
+                    b.ToTable("Diretor");
 
                     b.HasData(
                         new
                         {
                             Id = 1L,
-                            DataNascimento = new DateTime(2002, 9, 20, 0, 0, 0, 0, DateTimeKind.Local),
-                            DataRegistro = new DateTime(2022, 9, 20, 13, 59, 59, 653, DateTimeKind.Utc).AddTicks(7469),
+                            DataNascimento = new DateTime(2002, 9, 17, 0, 0, 0, 0, DateTimeKind.Local),
+                            DataRegistro = new DateTime(2022, 9, 17, 14, 51, 42, 667, DateTimeKind.Utc).AddTicks(6650),
                             Email = "diretor@portalescolar.com",
                             NomeCompleto = "Diretor",
                             Papel = 1,
@@ -70,51 +70,6 @@ namespace PortalEscolar.Infrastructure.Migrations
                         });
                 });
 
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.Diretoria.Matricula.Matricula", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bigint");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<long>("Id"), 1L, 1);
-
-                    b.Property<long>("AlunoId")
-                        .HasColumnType("bigint");
-
-                    b.Property<string>("CidadeNascimentoAluno")
-                        .IsRequired()
-                        .HasMaxLength(150)
-                        .HasColumnType("nvarchar(150)");
-
-                    b.Property<DateTime>("DataInicio")
-                        .HasColumnType("date");
-
-                    b.Property<DateTime>("DataNascimentoAluno")
-                        .HasColumnType("date");
-
-                    b.Property<DateTime>("DataRegistro")
-                        .HasColumnType("datetime2");
-
-                    b.Property<DateTime>("DataTerminio")
-                        .HasColumnType("date");
-
-                    b.Property<string>("NomeCompletoAluno")
-                        .IsRequired()
-                        .HasMaxLength(200)
-                        .HasColumnType("nvarchar(200)");
-
-                    b.Property<long>("TurmaId")
-                        .HasColumnType("bigint");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("AlunoId");
-
-                    b.HasIndex("TurmaId");
-
-                    b.ToTable("Matricula", (string)null);
-                });
-
             modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Aluno", b =>
                 {
                     b.Property<long>("Id")
@@ -122,9 +77,6 @@ namespace PortalEscolar.Infrastructure.Migrations
                         .HasColumnType("bigint");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<long>("Id"), 1L, 1);
-
-                    b.Property<long>("AlunoRA")
-                        .HasColumnType("bigint");
 
                     b.Property<DateTime>("DataNascimento")
                         .HasColumnType("date");
@@ -149,14 +101,9 @@ namespace PortalEscolar.Infrastructure.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<long?>("TurmaId")
-                        .HasColumnType("bigint");
-
                     b.HasKey("Id");
 
-                    b.HasIndex("TurmaId");
-
-                    b.ToTable("Aluno", (string)null);
+                    b.ToTable("Aluno");
                 });
 
             modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Professora", b =>
@@ -192,137 +139,7 @@ namespace PortalEscolar.Infrastructure.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Professora", (string)null);
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Turma", b =>
-                {
-                    b.Property<long>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("bigint");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<long>("Id"), 1L, 1);
-
-                    b.Property<bool>("Ativo")
-                        .HasColumnType("bit");
-
-                    b.Property<DateTime>("DataRegistro")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("NomeTurma")
-                        .IsRequired()
-                        .HasColumnType("varchar(50)")
-                        .HasColumnName("NomeTurma");
-
-                    b.Property<long>("ProfessoraId")
-                        .HasColumnType("bigint");
-
-                    b.Property<string>("Sala")
-                        .IsRequired()
-                        .HasColumnType("varchar(50)")
-                        .HasColumnName("Sala");
-
-                    b.Property<int>("Serie")
-                        .HasColumnType("int");
-
-                    b.Property<int>("Turno")
-                        .HasColumnType("int");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("ProfessoraId");
-
-                    b.ToTable("Turma", (string)null);
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.Diretoria.Matricula.Matricula", b =>
-                {
-                    b.HasOne("PortalEscolar.Domain.Entities.SalaAula.Aluno", "Aluno")
-                        .WithMany("Matricula")
-                        .HasForeignKey("AlunoId")
-                        .IsRequired();
-
-                    b.HasOne("PortalEscolar.Domain.Entities.SalaAula.Turma", "Turma")
-                        .WithMany("Matricula")
-                        .HasForeignKey("TurmaId")
-                        .IsRequired();
-
-                    b.OwnsOne("PortalEscolar.Domain.Entities.Diretoria.Matricula.Matricula.Responsavel#PortalEscolar.Domain.Entities.Diretoria.Matricula.Responsavel", "Responsavel", b1 =>
-                        {
-                            b1.Property<long>("MatriculaId")
-                                .HasColumnType("bigint");
-
-                            b1.Property<string>("CPF")
-                                .IsRequired()
-                                .HasColumnType("varchar(11)")
-                                .HasColumnName("ResponsavelCPF");
-
-                            b1.Property<string>("Cidade")
-                                .IsRequired()
-                                .HasColumnType("varchar(100)")
-                                .HasColumnName("ResponsavelCidade");
-
-                            b1.Property<DateTime>("DataNascimento")
-                                .HasColumnType("datetime2")
-                                .HasColumnName("ResponsavelDatanascimento");
-
-                            b1.Property<string>("NomeCompleto")
-                                .IsRequired()
-                                .HasColumnType("varchar(200)")
-                                .HasColumnName("ResponsavelNomecompleto");
-
-                            b1.Property<string>("Telefone")
-                                .IsRequired()
-                                .HasColumnType("varchar(20)")
-                                .HasColumnName("ResponsavelTelefone");
-
-                            b1.HasKey("MatriculaId");
-
-                            b1.ToTable("Matricula", (string)null);
-
-                            b1.WithOwner()
-                                .HasForeignKey("MatriculaId");
-                        });
-
-                    b.Navigation("Aluno");
-
-                    b.Navigation("Responsavel");
-
-                    b.Navigation("Turma");
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Aluno", b =>
-                {
-                    b.HasOne("PortalEscolar.Domain.Entities.SalaAula.Turma", null)
-                        .WithMany("Alunos")
-                        .HasForeignKey("TurmaId");
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Turma", b =>
-                {
-                    b.HasOne("PortalEscolar.Domain.Entities.SalaAula.Professora", "Professora")
-                        .WithMany("Turma")
-                        .HasForeignKey("ProfessoraId")
-                        .IsRequired();
-
-                    b.Navigation("Professora");
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Aluno", b =>
-                {
-                    b.Navigation("Matricula");
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Professora", b =>
-                {
-                    b.Navigation("Turma");
-                });
-
-            modelBuilder.Entity("PortalEscolar.Domain.Entities.SalaAula.Turma", b =>
-                {
-                    b.Navigation("Alunos");
-
-                    b.Navigation("Matricula");
+                    b.ToTable("Professora");
                 });
 #pragma warning restore 612, 618
         }
