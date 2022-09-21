8bfff3e17c24170615b380877387e3e997a7082f
JacksonSM <jacksons.m2018@gmail.com>
2022-09-21 20:29:00 -0300

    Removendo campo AlunoRA da classe de dominio Aluno.

--- src/backend/PortalEscolar.Domain/Entities/SalaAula/Aluno.cs
+++ src/backend/PortalEscolar.Domain/Entities/SalaAula/Aluno.cs
@@ -6,7 +6,6 @@ public class Aluno : Usuario
 {
     public string NomeCompleto { get; set; }
     public DateTime DataNascimento { get; set; }
-    public long AlunoRA { get; set; }
     public List<Matricula> Matricula { get; set; }
     public Aluno(){}
     public Aluno(string nomeCompleto, DateTime dataNascimento, string email, string senha) : base(email, senha)
