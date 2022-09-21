5365b5aa882fd914527b23209d61a2a211b0b9af
JacksonSM <jacksons.m2018@gmail.com>
2022-09-21 20:28:22 -0300

    Removendo endpoint registrar aluno.

--- src/backend/PortalEscolar.Api/Controllers/AlunoController.cs
+++ src/backend/PortalEscolar.Api/Controllers/AlunoController.cs
@@ -11,17 +11,6 @@ namespace PortalEscolar.Api.Controllers;
 [ApiController]
 public class AlunoController : ControllerBase
 {
-    [HttpPost]
-    [ProducesResponseType(typeof(GenericResponseJson), StatusCodes.Status201Created)]
-    [AutorizacaoPortalEscolar(new Papel[] { Papel.Diretor })]
-    public async Task<IActionResult> Registrar(
-    [FromServices] IRegistrarAlunoUseCase useCase,
-    [FromBody] RequestRegistrarAlunoJson request)
-    {
-        var resposta = await useCase.ExecuteAsync(request);
-
-        return Created(string.Empty, resposta);
-    }
     [HttpPost("login")]
     [ProducesResponseType(typeof(ResponseTokenJson), StatusCodes.Status200OK)]
     public async Task<IActionResult> Login(
