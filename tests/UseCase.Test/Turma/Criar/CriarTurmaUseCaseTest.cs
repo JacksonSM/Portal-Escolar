7cb0705dfb39f0cbb40fdd4f0bd056f775704aaf
JacksonSM <jacksons.m2018@gmail.com>
2022-09-21 09:57:04 -0300

    Feito testes para caso de uso de criar turma e o endpoint.

---
+++ tests/UseCase.Test/Turma/Criar/CriarTurmaUseCaseTest.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using PortalEscolar.Application.Responses;
+using PortalEscolar.Application.UseCases.Turma.Criar;
+using PortalEscolar.Exceptions;
+using PortalEscolar.Exceptions.ExceptionsBase;
+using Utilities.Repositories;
+using Utilities.Repositories.Professora;
+using Utilities.Repositories.Turma;
+using Utilities.Requests;
+using Utilities.Services.Mapper;
+using Xunit;
+
+namespace UseCase.Test.Turma.Criar;
+public class CriarTurmaUseCaseTest
+{
+    [Fact]
+    public async void UseCase_DadosValidos_DeveRetornaGenericResponseJsonComMensagemDeSucesso()
+    {
+        var request = RequestCriarTurmaJsonBuilder.Build();
+        var useCase = BuildUseCase(request.ProfessoraId);
+        var result = await useCase.ExecuteAsync(request);
+
+        result.Should().NotBeNull();
+        result.Mensagem.Should().NotBeNullOrWhiteSpace();
+        result.Mensagem.Should().Contain(ResourceRespostasUseCases.TURMA_CRIADA_COM_SUCESSO);
+    }
+    [Fact]
+    public async void ProfessoraId_IdInexistente_DeveLancarExceptionProfessoraInexistente()
+    {
+        var request = RequestCriarTurmaJsonBuilder.Build();
+        var useCase = BuildUseCase();
+
+        Func<Task> action = async () => { await useCase.ExecuteAsync(request); };
+
+        await action.Should().ThrowAsync<ErrosDeValidacaoException>()
+            .Where(erro => erro.MensagensDeErro.Count == 1 && 
+                erro.MensagensDeErro.Contains(ResourceMensagensDeErro.PROFESSORA_NAO_ENCONTRADA));
+    }
+
+    private CriarTurmaUseCase BuildUseCase(long id = 0)
+    {
+        var turmaWriteRepo = TurmaWriteOnlyRepositoryBuilder.Instance().Build();
+        var professoraReadRepo = ProfessoraReadOnlyRepositoryBuilder.Instance().ExisteProfessora(id).Build();
+        var mapper = AutoMapperBuilder.Instance();
+        var unit = UnitOfWorkBuilder.Instance().Build();
+
+
+        var useCase = new CriarTurmaUseCase(turmaWriteRepo,unit,mapper,professoraReadRepo);
+        return useCase;
+    }
+}
---
+++ tests/WebApi.Test/Diretor/CriarTurmaTest.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using PortalEscolar.Application.Responses;
+using PortalEscolar.Exceptions;
+using System.Net;
+using System.Text.Json;
+using Utilities.Requests;
+using Utilities.Services.Token;
+using Xunit;
+
+namespace WebApi.Test.Diretor;
+public class CriarTurmaTest : ControllerBase
+{
+    private const string METODO = "api/diretor/criar-turma";
+
+    private PortalEscolar.Domain.Entities.Diretoria.Diretor _diretor;
+    private PortalEscolar.Domain.Entities.SalaAula.Professora _professora;
+    public CriarTurmaTest(PortalEscolarWebApplicationFactory<Program> factory) : base(factory)
+    {
+        _diretor = factory.ObterDiretor();
+        _professora = factory.ObterProfessora();
+    }
+
+    [Fact]
+    public async Task EndPointCriarTurma_DadosValido_StatusCreated()
+    {
+        var request = RequestCriarTurmaJsonBuilder.Build();
+        request.ProfessoraId = _professora.Id;
+
+        var tokenController = TokenControllerBuilder.Instance();
+
+        var token = tokenController.GerarToken(_diretor);
+
+        var response = await PostRequest(METODO, request, token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        await using var responstaBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responstaBody);
+
+        responseData.RootElement.GetProperty("mensagem").GetString().Should()
+            .NotBeNullOrWhiteSpace().And.Contain(ResourceRespostasUseCases.TURMA_CRIADA_COM_SUCESSO);
+    }
+    [Fact]
+    public async Task Professora_ProfessoraInexistente_StatusBadRequest()
+    {
+        var request = RequestCriarTurmaJsonBuilder.Build();
+        request.ProfessoraId = 9563;
+
+        var tokenController = TokenControllerBuilder.Instance();
+
+        var token = tokenController.GerarToken(_diretor);
+
+        var response = await PostRequest(METODO, request, token);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        await using var responstaBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responstaBody);
+
+        var erros = responseData.RootElement.GetProperty("mensagens").EnumerateArray();
+        erros.Should().ContainSingle().And.Contain(c => c.GetString()
+            .Equals(ResourceMensagensDeErro.PROFESSORA_NAO_ENCONTRADA));
+    }
+}
