3455148413899e756139d1057a0cb22e3dd4f5e1
JacksonSM <jacksons.m2018@gmail.com>
2022-09-27 10:27:40 -0300

    Feito testes para o validator do caso de uso enviar exercicio.

---
+++ tests/Utilities/Requests/RequestEnviarExercicioJsonBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using PortalEscolar.Communication.Request.Exercicio;
+using PortalEscolar.Domain.Enum;
+
+namespace Utilities.Requests;
+public class RequestEnviarExercicioJsonBuilder
+{
+    public static RequestEnviarExercicioJson Build()
+    {
+        var exercicioFake = new Faker<RequestEnviarExercicioJson>()
+            .RuleFor(c => c.PrazoEntrega, f => f.Person.DateOfBirth.Date.ToString("dd/MM/yyyy HH:mm"))
+            .RuleFor(c => c.Nome, f => f.Person.FullName)
+            .RuleFor(c => c.Disciplina, f => f.PickRandom<Disciplina>()).Generate();
+
+        var questoesFakes = new Faker<QuestoesExercicioJson>()
+            .RuleFor(c => c.Ordem, f => f.IndexFaker+1)
+            .RuleFor(c => c.Enunciado, f => f.Lorem.Paragraph())
+            .RuleFor(c => c.Alternativas, f => f.Make(4, () => f.Lorem.Lines()))
+            .RuleFor(c => c.AlternativaCorreta, f => f.Lorem.Letter()).Generate(5);
+
+        exercicioFake.Questoes = questoesFakes;
+
+        return exercicioFake;
+    }
+}
---
+++ tests/Validators.Test/Professora/EnviarExercicioValidator.cs
@@ -0,0 +1,156 @@
+﻿using FluentAssertions;
+using PortalEscolar.Application.UseCases.Professora.EnviarExercicio;
+using PortalEscolar.Domain.Enum;
+using PortalEscolar.Exceptions;
+using Utilities.Requests;
+using Xunit;
+
+namespace Validators.Test.Professora;
+public class EnviarExercicioValidator
+{
+    [Fact]
+    public void Objeto_ParametrosValidos_ObjetoValido()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void PrazoEntrega_CampoVazio_ErroCampoVazio()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.PrazoEntrega = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_PRAZOENTREGA_VAZIO));
+    }
+    [Fact]
+    public void PrazoEntrega_CampoInvalido_ErroCampoInvalido()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.PrazoEntrega = "12/366/2009 15:69";
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_DATAENTREGA_INVALIDO));
+    }
+    [Fact]
+    public void PrazoEntrega_CampoComDadoValido_CampoValido()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeTrue();
+    }
+    [Fact]
+    public void Nome_CampoVazio_ErroCampoVazio()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.Nome = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_NOME_VAZIO));
+    }
+    [Fact]
+    public void Nome_CampoComMais200Caracteres_ErroComMais200Caracteres()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.Nome = string.Join("" ,Enumerable.Repeat("a", 203)); 
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_NOME_NOMAXIMO200CARACTERES));
+    }
+    [Fact]
+    public void Disciplina_CampoInvalido_ErroCampoInvalido()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.Disciplina =(Disciplina) 9999;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_DISCIPLINA_INEXISTENTE));
+    }
+    [Fact]
+    public void Questoes_QuestoesComOrdemRepetidas_ErroQuestoesComOrdemRepetidas()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+
+        request.Questoes[0].Ordem = 1;
+        request.Questoes[1].Ordem = 1;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_QUESTAO_ORDEM_REPETIDAS));
+    }
+    [Fact]
+    public void Enunciado_CampoVazio_ErroCampoVazio()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+        request.Questoes[0].Enunciado = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_QUESTAO_ENUNCIADO_VAZIO));
+    }
+    [Fact]
+    public void Alternativas_CampoComMenos2Alternativas_ErroCampoComMenos2Alternativas()
+    {
+        var validator = new EnviarExercicioValidador();
+
+        var request = RequestEnviarExercicioJsonBuilder.Build();
+
+        request.Questoes[0].Alternativas.RemoveRange(1, request.Questoes[0].Alternativas.Count - 1);
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should()
+            .ContainSingle().And
+            .Contain(erros => erros.ErrorMessage.Equals(ResourceMensagensDeErro.ENVIAR_EXERCICIO_QUESTAO_ALTERNATIVAS_INVALIDO));
+    }
+}
